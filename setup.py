d9cccdd9d9ac38e7c390d00931068cb6bf66a7c0
Alberto Villar <alberto@writefull.com>
2021-05-26 18:28:38 +0200

    Add the repo url to setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A Python library for the Writefull API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/writefull/writefull-python",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
