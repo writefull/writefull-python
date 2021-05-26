5f862ed3748940938cd0f23777cf1c9f95d6fb46
Alberto Villar <alberto@writefull.com>
2021-05-26 18:25:08 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="writefull",
+    version="0.0.0",
+    author="Writefull",
+    author_email="support@writefull.com",
+    description="A Python library for the Writefull API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
